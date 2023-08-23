150f1e5ab7d00a35894c32e5e29f5ae3712f3b8d
UlrichBR <38494475+UlrichBR@users.noreply.github.com>
2023-08-22 21:53:09 -0300

    Update RegionImplement.java

--- src/main/java/me/ulrich/clans/interfaces/RegionImplement.java
+++ src/main/java/me/ulrich/clans/interfaces/RegionImplement.java
@@ -4,12 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 
 public interface RegionImplement {
 	
-	boolean hasRegion(String regionName);
+	boolean hasRegion(String regionName, Location location);
 	
 	boolean isPVPAllowed(Player player, Location location);
 	
@@ -20,6 +21,10 @@ public interface RegionImplement {
 	List<String> getPlayerRegion(Player player);
 	
 	boolean hasBuildPermission(Player player, Block block);
+	
+	List<String> getLocationRegion(Location loc);
+	
+	List<String> getWorldRegionList(World world);
 	
 	Optional<String> getPluginVersion();
 
