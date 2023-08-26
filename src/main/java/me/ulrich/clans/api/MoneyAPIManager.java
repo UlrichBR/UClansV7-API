14b0ee74e2e5094ff242a709b76eab490f578934
UlrichBR <38494475+UlrichBR@users.noreply.github.com>
2023-08-26 16:22:19 -0300

    Update MoneyAPIManager.java

--- src/main/java/me/ulrich/clans/api/MoneyAPIManager.java
+++ src/main/java/me/ulrich/clans/api/MoneyAPIManager.java
@@ -1,32 +1,59 @@
 package me.ulrich.clans.api;
 
-import org.bukkit.entity.Player;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Optional;
 
 import me.ulrich.clans.Clans;
 import me.ulrich.clans.interfaces.MoneyAPI;
+import me.ulrich.clans.interfaces.MoneyImplement;
 
 public class MoneyAPIManager implements MoneyAPI {
+
+	public MoneyAPIManager(Clans plugin) {
+		
+	}
 	
-	public MoneyAPIManager(Clans plugin) {}
+	@Override
+	public Optional<MoneyImplement> getPreferentialOrFirstImplement() {
+		return Optional.empty();
+	}
 
 	@Override
-	public boolean hasMoney(Player player, int amount) {
+	public HashMap<String, MoneyImplement> getImplementedMoney() {
+		return null;
+	}
+
+	@Override
+	public boolean addImplementation(String pluginName, MoneyImplement region) {
 		return false;
 	}
 
 	@Override
-	public boolean addMoney(Player player, int amount) {
+	public boolean hasPluginImplemented(String pluginName) {
 		return false;
 	}
 
 	@Override
-	public boolean withdrawMoney(Player player, int amount) {
+	public boolean removeImplementation(String pluginName) {
 		return false;
 	}
 
 	@Override
-	public boolean isEnabled() {
-		return false;
+	public Optional<MoneyImplement> getMoneyImplemented(String pluginName) {
+		return Optional.empty();
 	}
+
+	@Override
+	public List<String> getImplementationPluginsNames() {
+		return null;
+	}
+
+	@Override
+	public HashMap<String, MoneyImplement> getImplementationPluginsData() {
+		return null;
+	}
+	
+
 	
 }
