a1401b6924a3831acbd5f5a7edafc1a7c2b7100b
UlrichBR <38494475+UlrichBR@users.noreply.github.com>
2023-09-07 12:03:19 -0300

    Update LibAPIManager.java

--- src/main/java/me/ulrich/clans/api/LibAPIManager.java
+++ src/main/java/me/ulrich/clans/api/LibAPIManager.java
@@ -1,6 +1,13 @@
 package me.ulrich.clans.api;
 
+import java.util.HashMap;
+import java.util.Optional;
+import java.util.UUID;
+
 import org.bukkit.entity.Player;
+
+import com.github.Anon8281.universalScheduler.scheduling.schedulers.TaskScheduler;
+import com.github.Anon8281.universalScheduler.scheduling.tasks.MyScheduledTask;
 
 import me.ulrich.clans.Clans;
 import me.ulrich.clans.data.ClanEnum.TranslatableKey;
@@ -51,6 +58,84 @@ public class LibAPIManager implements LibAPI {
 	@Override
 	@Deprecated
 	public String translateOutReference(String key, String object, String... args) {
+		return null;
+	}
+
+	@Override
+	public boolean isTaskInitialized() {
+		
+		return false;
+	}
+
+	@Override
+	public boolean hasBossbarTask(UUID playerUUID) {
+		
+		return false;
+	}
+
+	@Override
+	public Optional<MyScheduledTask> getBossbarTask(UUID playerUUID) {
+		
+		return Optional.empty();
+	}
+
+	@Override
+	public boolean stopBossbarTask(UUID playerUUID) {
+		
+		return false;
+	}
+
+	@Override
+	public Optional<MyScheduledTask> createBossbarTask(UUID playerUUID, MyScheduledTask task) {
+		
+		return Optional.empty();
+	}
+
+	@Override
+	public boolean hasLibTask(UUID playerUUID) {
+		
+		return false;
+	}
+
+	@Override
+	public Optional<MyScheduledTask> getLibTask(UUID playerUUID) {
+		
+		return Optional.empty();
+	}
+
+	@Override
+	public boolean stopLibTask(UUID playerUUID) {
+		
+		return false;
+	}
+
+	@Override
+	public Optional<MyScheduledTask> createLibTask(UUID playerUUID, MyScheduledTask task) {
+		
+		return Optional.empty();
+	}
+
+	@Override
+	public boolean isFolia() {
+		
+		return false;
+	}
+
+	@Override
+	public TaskScheduler getScheduler() {
+		
+		return null;
+	}
+
+	@Override
+	public HashMap<UUID, MyScheduledTask> getBossbarTasks() {
+		
+		return null;
+	}
+
+	@Override
+	public HashMap<UUID, MyScheduledTask> getLibTasks() {
+		
 		return null;
 	}
 
