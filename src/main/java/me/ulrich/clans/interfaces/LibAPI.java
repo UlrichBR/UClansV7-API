052179d280887cd1cfbf2e57aeb9698ede6ac589
UlrichBR <38494475+UlrichBR@users.noreply.github.com>
2023-09-07 12:03:01 -0300

    Update LibAPI.java

--- src/main/java/me/ulrich/clans/interfaces/LibAPI.java
+++ src/main/java/me/ulrich/clans/interfaces/LibAPI.java
@@ -1,13 +1,18 @@
 package me.ulrich.clans.interfaces;
 
+import java.util.HashMap;
+import java.util.Optional;
+import java.util.UUID;
+
 import org.bukkit.entity.Player;
+
+import com.github.Anon8281.universalScheduler.scheduling.schedulers.TaskScheduler;
+import com.github.Anon8281.universalScheduler.scheduling.tasks.MyScheduledTask;
 
 import me.ulrich.clans.data.ClanEnum.TranslatableKey;
 import net.kyori.adventure.platform.bukkit.BukkitAudiences;
 
 public interface LibAPI {
-
-	
 
 	void sendTitle(Player p, String msg, int fadeIn, int stay, int fadeOut);
 
@@ -26,5 +31,31 @@ public interface LibAPI {
 	String translate(TranslatableKey trans, String object, String... args);
 
 	String translateOutReference(String key, String object, String... args);
+	
+	boolean isTaskInitialized();
+	
+	boolean hasBossbarTask(UUID playerUUID);
+	
+	Optional<MyScheduledTask> getBossbarTask(UUID playerUUID);
+	
+	boolean stopBossbarTask(UUID playerUUID);
+	
+	Optional<MyScheduledTask> createBossbarTask(UUID playerUUID, MyScheduledTask task);
+	
+	boolean hasLibTask(UUID playerUUID);
+	
+	Optional<MyScheduledTask> getLibTask(UUID playerUUID);
+	
+	boolean stopLibTask(UUID playerUUID);
+	
+	Optional<MyScheduledTask> createLibTask(UUID playerUUID, MyScheduledTask task);
+	
+	boolean isFolia();
+	
+	TaskScheduler getScheduler();
+	
+	HashMap<UUID, MyScheduledTask> getBossbarTasks();
+	
+	HashMap<UUID, MyScheduledTask> getLibTasks();
 	
 }
