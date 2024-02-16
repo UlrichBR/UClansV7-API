5c1a1529ec952d94efc343a90dfc9742c37c86fb
UlrichBR <38494475+UlrichBR@users.noreply.github.com>
2024-02-16 13:20:40 -0300

    Update SyncAPIManager.java

--- src/main/java/me/ulrich/clans/api/SyncAPIManager.java
+++ src/main/java/me/ulrich/clans/api/SyncAPIManager.java
@@ -1,11 +1,14 @@
 package me.ulrich.clans.api;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import org.bukkit.entity.Player;
 
 import me.ulrich.clans.Clans;
 import me.ulrich.clans.data.ClanEnum.RedisType;
+import me.ulrich.clans.data.OnlineData;
 import me.ulrich.clans.interfaces.SyncAPI;
 
 public class SyncAPIManager implements SyncAPI {
@@ -35,15 +38,6 @@ public class SyncAPIManager implements SyncAPI {
 
 	}
 	
-	@Override
-	public List<String> getProxyedPlayers() {
-		return null;
-	}
-
-	@Override
-	public void setProxyedPlayers(List<String> proxyedPlayers) {
-
-	}
 	
 	@Override
 	public void debugMessage(DebugType type, String message) {
@@ -52,5 +46,35 @@ public class SyncAPIManager implements SyncAPI {
 	
 	public Clans getPlugin() {
 		return plugin;
+	}
+
+	@Override
+	public void clearOnlineData() {
+		
+	}
+
+	@Override
+	public Optional<OnlineData> getOnlineData(UUID playerUUID) {
+		return Optional.empty();
+	}
+
+	@Override
+	public Optional<OnlineData> getOnlineData(String playerNickname) {
+		return Optional.empty();
+	}
+
+	@Override
+	public boolean addOnlineData(UUID playerUUID, String nickname, String server) {
+		return false;
+	}
+
+	@Override
+	public boolean removeOnlineData(UUID playerUUID) {
+		return false;
+	}
+
+	@Override
+	public void solveProxiedPlayers(String jsonArray) {
+		
 	}
 }
