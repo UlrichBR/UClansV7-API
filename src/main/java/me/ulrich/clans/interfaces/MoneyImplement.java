34fd8633c57ed159317d71ace402b83890b6f4f6
UlrichBR <38494475+UlrichBR@users.noreply.github.com>
2024-06-23 12:21:14 -0300

    Update MoneyImplement.java

--- src/main/java/me/ulrich/clans/interfaces/MoneyImplement.java
+++ src/main/java/me/ulrich/clans/interfaces/MoneyImplement.java
@@ -6,13 +6,22 @@ import org.bukkit.entity.Player;
 
 public interface MoneyImplement {
 	
+	@Deprecated
 	boolean hasMoney(Player player, int amount);
-
+	@Deprecated
 	boolean addMoney(Player player, int amount);
-
+	@Deprecated
 	boolean withdrawMoney(Player player, int amount);
-
+	@Deprecated
 	int getMoney(Player player);
+	
+	boolean hasMoney(Player player, double amount);
+
+	boolean addMoney(Player player, double amount);
+
+	boolean withdrawMoney(Player player, double amount);
+
+	double getMoney(Player player, boolean inDouble);
 	
 	boolean isInternal();
 	
